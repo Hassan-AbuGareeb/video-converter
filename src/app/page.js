9251c49c0caebe77ecd77b67e8fb8b60cd8cda99
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-12-21 19:40:16 +0300

    added some enhancments to buttons and enabled fullscreen mode for videos

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,50 @@
 "use client";
 import { useEffect, useState } from "react";
 import Search from "@/util/Search";
+import Button from "@/util/Button";
+
+export default function Home() {
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    search &&
+      getSearchResults(search).then((videos) => setSearchResults(videos.items));
+  }, [search]);
+
+  const videoCards = searchResults.map((video) => {
+    return (
+      <div
+        key={video.id.videoId}
+        className="flex felx-col flex-wrap justify-center w-[300px] p-2 bg-slate-500 border border-teal-500 rounded-lg shadow"
+      >
+        <iframe
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          allowFullScreen={true}
+          className="rounded-lg w-full h-48"
+        />
+        <div className="px-auto py-2 text-center">
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
+            {video.snippet.title}
+          </h5>
+          <div className="flex flex-wrap gap-x-5 justify-center py-3">
+            <Button buttonText={"Download"} callBack={downloadFile} />
+            <Button buttonText={"Convert to MP3"} callBack={convertVideo} />
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="">
+      <Search onSearchClick={setSearch} />
+      <div className="flex flex-row flex-wrap w-full p-5 justify-center gap-10">
+        {videoCards}
+      </div>
+    </div>
+  );
+}
 
 async function getSearchResults(videoTitle) {
   const options = {
@@ -16,41 +60,6 @@ async function getSearchResults(videoTitle) {
   return searchResultsData;
 }
 
-export default function Home() {
-  const [search, setSearch] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+function downloadFile() {}
 
-  useEffect(() => {
-    search &&
-      getSearchResults(search).then((videos) => setSearchResults(videos.items));
-  }, [search]);
-
-  const videoCards = searchResults.map((video) => {
-    return (
-      <div
-        key={video.id.videoId}
-        className="w-[300px] p-2 bg-slate-500 border border-teal-500 rounded-lg shadow"
-      >
-        <iframe
-          src={`https://www.youtube.com/embed/${video.id.videoId}`}
-          className="rounded-lg w-full h-60"
-        />
-        <div className="px-auto py-2 text-center">
-          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">
-            {video.snippet.title}
-          </h5>
-          {/* add two buttons here download, and convert to mp3*/}
-        </div>
-      </div>
-    );
-  });
-  // onSearchClick={setSearch}
-  return (
-    <div className="">
-      <Search onSearchClick={setSearch} />
-      <div className="flex flex-row flex-wrap w-full p-5 justify-center gap-10">
-        {videoCards}
-      </div>
-    </div>
-  );
-}
+function convertVideo() {}
